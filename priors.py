8fcb0bec30f5733dcdd86e5177b62c632af3d37a
Mat Leonard <leonard.mat@gmail.com>
2015-06-26 20:50:48 -0700

    Changed out of bounds value to -inf.

--- priors.py
+++ priors.py
@@ -4,18 +4,20 @@
 """
 from core import np
 
+OUTOFBOUNDS = -np.inf
+
 
 def uniform(x, lower=0., upper=1.):
     if x < lower:
-        return -100000.
+        return OUTOFBOUNDS
     elif x > upper:
-        return -100000.
+        return OUTOFBOUNDS
     else:
         return -np.log(upper-lower)
 
 
 def poisson(events, lam):
     if lam <= 0:
-        return -100000.
+        return OUTOFBOUNDS
     else:
         return np.sum(events*np.log(lam)) - events.size*lam
