a4cd001f3329dccc1eaff5898bf0dc1a6608f7b9
Andrew Miller <andrew.colin.miller@gmail.com>
2015-07-01 18:59:51 -0700

    added gibbs sample to example/acf

--- examples/effective_sample_size_example.py
+++ examples/effective_sample_size_example.py
@@ -15,24 +15,35 @@ def logp(x, y):
     return -.5 * np.dot(np.dot(d, icov), d)
 #logp_xy = lambda(th): logp(th[0], th[1])
 
-# compare the performance of NUTS and Metropolis by effective sample size
+# NUTS sample
 nuts = smp.NUTS(logp)
 nuts_trace = nuts.sample(1000)
 
+# Metropolis Hastings
 met = smp.Metropolis(logp)
 met_trace = met.sample(1000)
+
+# gibbs sampler (NUTS-within-gibbs)
+step1 = smp.Metropolis(logp, condition=['y']) # samples x & y, given z
+step2 = smp.NUTS(logp, condition=['x']) # samples z, given x & y
+chain = smp.Chain([step1, step2])
+gibbs_trace = chain.sample(1000)
 
 # compute effective sample size based on autocorrelation
 nuts_eff = diagnostics.compute_n_eff_acf(nuts_trace.x)
 met_eff = diagnostics.compute_n_eff_acf(met_trace.x)
+gibbs_eff = diagnostics.compute_n_eff_acf(gibbs_trace.x)
 print "NUTS effective sample size: %2.2f"%nuts_eff
 print "MH   effective sample size: %2.2f"%met_eff
+print "Gibbs effective sample size: %2.2f"%gibbs_eff
 
 # graphically compare samples
-fig, axarr = plt.subplots(1, 2)
+fig, axarr = plt.subplots(1, 3, figsize=(15, 6))
 axarr[0].scatter(nuts_trace.x, nuts_trace.y)
 axarr[0].set_title("NUTS samples")
 axarr[1].scatter(met_trace.x, met_trace.y)
 axarr[1].set_title("MH samples")
+axarr[2].scatter(gibbs_trace.x, gibbs_trace.y)
+axarr[2].set_title("Gibbs samples")
 plt.show()
 
