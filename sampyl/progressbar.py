a2255aa7f1778e04a2ac57cd05d0c34c9a915249
Mat Leonard <leonard.mat@gmail.com>
2015-07-14 11:49:33 -0700

    Added a progress bar to show some feedback while sampling.

---
+++ sampyl/progressbar.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def update_progress(current, total, width=30, end=False):
+    bar_width = width
+    block = int(round(bar_width * current/total))
+    text = "\rProgress: [{0}] {1} of {2} samples".\
+             format("#"*block + "-"*(bar_width-block), current, total)
+    if end:
+        text = text +'\n'
+    sys.stdout.write(text)
+    sys.stdout.flush()
