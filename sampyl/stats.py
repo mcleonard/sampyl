dbde07a4c1976219cfe8d43e2e2226a368fa8c06
Mat Leonard <leonard.mat@gmail.com>
2016-05-05 10:50:19 -0700

    Add percentile function in stats module.

--- sampyl/stats.py
+++ sampyl/stats.py
@@ -1,13 +1,22 @@
-""" Module for statistical  """
+""" Module for statistical calculations """
 
 from .core import np
 
 
-def hpd(trace):
+def hpd(chain):
     pass
 
-def percentile(trace):
+def percentile(chain, alpha=0.95):
+    q = 100*(1 - alpha)/2.
+    f = lambda x: np.percentile(x, q=(q, 100 - q), axis=0).T
+    fields = chain.dtype.fields.keys()
+    return {field: f(chain.field(field)) for field in fields}
+
+def autocorr(chain):
     pass
 
-def autocorrrelation(trace):
+def R_hat(chain):
+    pass
+
+def effective_samples(chain):
     pass
